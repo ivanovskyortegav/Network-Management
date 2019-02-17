adc04797bbb34631d819aaf589e7e5b3e2752cab
Ivan Ortega Victoriano <47719401+ivanovskyortegav@users.noreply.github.com>
2019-02-17 11:59:45 -0600

    Ethernet interfaces script for lb2

---
+++ loadbalancers/lb2/eth-interfaces.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+ifconfig eth0 up # Configurada por Zebra
+ifconfig eth1 up # Configurada por Zebra
+ifconfig eth2 up # Configurada por Keepalived
+ifconfig eth3 up # Configurada por Keepalived
+ifconfig eth4 up # Configurada por Zebra
+ifconfig eth5 up # Configurada por Zebra
